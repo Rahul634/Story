082cc647c93fb75d0c026a3f87de8d6e92b2b1a1
Rahul Bhave <bhavemona@gmail.com>
2021-10-10 03:13:58 +0530

    passport js and google auth done

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDb = require('./config/db')
 dotenv.config({
     path: './config/config.env'
@@ -15,10 +17,24 @@ const app = express()
 if(process.env.NODE_ENV == 'developement'){
     app.use(morgan('dev'))
 }
+
+// passport
+require('./config/passport')(passport)
+
 //handlebars
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs')
 
+//session
+app.use(session({
+    secret: 'boring',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure : true}
+}))
+//passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 //static
 app.use(express.static(path.join(__dirname, 'public')))
 
